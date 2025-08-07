6f850c05125255ba80e3259f688f782fcdb002ca
Shivam Singh <shivamsinghsn95@gmail.com>
2025-08-07 14:20:15 +0530

    get used

--- api/index.js
+++ api/index.js
@@ -33,6 +33,9 @@ app.use(express.json());
 app.use(cookieParser());
 
 // API Routes
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/post', postRoutes);
